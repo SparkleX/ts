b310f9c1abaccc3a99641e5391591b2577391a7d
sunxufei <sunxufei@hotmail.com>
2019-07-11 21:54:13 +0800

    add

--- help/sqlite-js/test.js
+++ help/sqlite-js/test.js
@@ -2,15 +2,15 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
  
 db.serialize(function() {
-  db.run('CREATE TABLE "lorem" (info TEXT)');
+  db.run('CREATE TABLE "AppleStore" (info TEXT)');
  
-  var stmt = db.prepare('INSERT INTO "lOrem" VALUES (?)');
+  var stmt = db.prepare('INSERT INTO applestore VALUES (?)');
   for (var i = 0; i < 10; i++) {
       stmt.run("Ipsum " + i);
   }
   stmt.finalize();
  
-  db.each("SELECT * FROM lorem", function(err, row) {
+  db.each('SELECT * FROM Applestore', function(err, row) {
       console.log(row);
   });
 });
