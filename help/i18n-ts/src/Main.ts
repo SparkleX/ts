3b0e6b21e890eceecd8b41eea782bdee82e620a7
SparkleX <sunxufei@hotmail.com>
2019-07-17 10:21:28 +0800

    add

---
+++ help/i18n-ts/src/Main.ts
@@ -0,0 +1,9 @@
+import * as i18n from "i18n";
+
+i18n.configure({
+    locales:['en', 'cn'],
+    defaultLocale: 'cn',
+    directory: __dirname + '/i18n'
+});
+var greeting = i18n.__('Hello');
+console.debug(greeting);
---
+++ help/mocha-ts/test/Test.ts
@@ -0,0 +1,33 @@
+import hello from './hello';
+import { expect } from 'chai';
+//import { suite, test, slow, timeout } from "mocha";
+
+// if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
+// import 'mocha';
+
+describe('Main Test', () => {
+    before(function() {
+        console.log('before');
+    });
+    after(function() {
+        console.log('after');
+    });
+    beforeEach(function() {
+        console.log('beforeEach');
+    });
+    afterEach(function() {
+        console.log('afterEach');
+    });    
+    describe('Test1', () => {
+        it('Success Sample', () => {
+            const result = hello();
+            expect(result).to.equal('Hello World!');
+        });
+    });
+    describe('Test2', () => {
+        it('Fail Sample', () => {
+            const result = hello();
+            expect(result).not.equal('Hello World!');
+        });
+    });
+});
