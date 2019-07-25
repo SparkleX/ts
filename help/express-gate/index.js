d0e65c68c1ff8ae8bcf5f23a70c55bdfbe402f2f
SparkleX <sunxufei@hotmail.com>
2019-07-25 12:36:52 +0800

    add

--- help/express-gate/index.js
+++ help/express-gate/index.js
@@ -3,10 +3,13 @@ const morgan = require("morgan");
 const compression = require("compression");
 const proxy = require('http-proxy-middleware')
 const session = require('express-session')
+const spdy = require('spdy')
+const path = require('path')
+const fs = require('fs')
 
 var app = express()
 
-app.use(session({ secret: 'keyboard cat', cookie: { maxAge: 60000 }}))
+app.use(session({ secret: 'keyboard cat'}))
 app.use(morgan("dev"));
 app.use(compression());
 app.use(express.static('public'));
@@ -15,7 +18,7 @@ app.use('/auth', proxy({ target: 'http://localhost:9901', changeOrigin: true , o
 app.use('/tcli', proxy({ target: 'http://localhost:9902', changeOrigin: true , onProxyReq: onProxyReq}));
 app.use('/webx', proxy({ target: 'http://localhost:8080', changeOrigin: true }));
 
-app.listen(3000);
+//app.listen(3000);
 function onProxyReq(proxyReq, req, res) {
 	if(req.session.jwt) {
 		proxyReq.setHeader('authorization', req.session.jwt);
@@ -29,3 +32,20 @@ function onProxyRes(proxyRes, req, res) {
 		req.session.jwt = auth;
 	}
   }
+
+
+
+const options = {
+    key: fs.readFileSync(__dirname + '/server.key'),
+    cert:  fs.readFileSync(__dirname + '/server.crt')
+};
+const port = 3000;
+spdy.createServer(options, app)
+	.listen(port, (error) => {
+	if (error) {
+		console.error(error);
+		return process.exit(1);
+    } else {
+		console.log('Listening on port: ' + port + '.');
+    }
+  });
