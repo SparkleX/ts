585d9188f2273a6ed00898d14d01233c5a98178f
SparkleX <sunxufei@hotmail.com>
2019-12-30 17:28:12 +0800

    dev

--- vue-widget/test/unit/LabelInput.spec.js
+++ vue-widget/test/unit/LabelInput.spec.js
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils";
 import LabelInput from "../../src/widget/LabelInput.vue";
 
 //https://github.com/vuejs/vue-test-utils-getting-started/blob/master/test.js
-describe("LabelInput", () => {
+/*describe("LabelInput", () => {
 	// describe 代表一个作用域
 	it("create", () => {
 		const wrapper = mount(LabelInput, {
@@ -14,4 +14,14 @@ describe("LabelInput", () => {
 		expect(wrapper.html()).toContain('my label');
 		//expect(wrapper.vm.visible).toBe(true);
 	});
+});*/
+test("create", () => {
+	const wrapper = mount(LabelInput, {
+		propsData: {
+			label: "my label"
+		}
+	});
+	console.debug(wrapper.html());
+	expect(wrapper.html()).toContain('my label');
+	//expect(wrapper.vm.visible).toBe(true);
 });
